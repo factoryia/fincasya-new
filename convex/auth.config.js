f5b064a6f3371e8acc3fd71e19bb42bd4369a57c
James-Galvis <galvisrodriguez618@gmail.com>
2026-04-08 00:55:33 -0500

    fix: cleanup compiled files in convex folder and update gitignore

--- convex/auth.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_config_1 = require("@convex-dev/better-auth/auth-config");
-exports.default = {
-    providers: [(0, auth_config_1.getAuthConfigProvider)()],
-};
-//# sourceMappingURL=auth.config.js.map
