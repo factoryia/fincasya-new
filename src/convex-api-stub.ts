58ad74e9e14eb86a90fee2514e58da5b311418a7
James-Galvis <galvisrodriguez618@gmail.com>
2026-04-08 00:37:42 -0500

    fix: deploy

--- src/convex-api-stub.ts
+++ src/convex-api-stub.ts
@@ -5,6 +5,13 @@
 import * as path from 'path';
 
 const generatedPath = path.join(__dirname, '../convex/_generated/api');
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const generated = require(generatedPath);
+let generated: any = { api: {} };
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  generated = require(generatedPath);
+} catch (e) {
+  console.warn(`[api] Warning: Could not load Convex API from ${generatedPath}. Runtime queries may fail.`);
+}
+
 export const api = generated.api ?? generated;
